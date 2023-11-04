c0f94f9700fa501884265cf2839fe183a5b9654a
Filip Jirsák <filip@jirsak.org>
2023-11-04 13:29:36 +0100

    fix: zalomení zadání na max 100 znaků

--- src/components/Ukol3/index.jsx
+++ src/components/Ukol3/index.jsx
@@ -1,8 +1,12 @@
 import { useState } from 'react';
 
-// Zadání 1: Přidejte na prvek `input` událost `onChange`, která bude do stavu `login` ukládat aktuální hodnotu, kterou uživatel do inputu napíše. Zároveň nastavte `value` políčka na hodnotu ze stavu `login`, takže při načtení stránky v políčku uvidíte výchozí hodnotu `petr`.
-
-// Zadání 2: Na tlačíko přidejte událost `onClick`, která nastaví stav `login` na prázdný řetězec, čímž se vyprázdní i políčko s loginem.
+/*
+Zadání 1: Přidejte na prvek `input` událost `onChange`, která bude do stavu `login` ukládat
+  aktuální hodnotu, kterou uživatel do inputu napíše. Zároveň nastavte `value` políčka
+  na hodnotu ze stavu `login`, takže při načtení stránky v políčku uvidíte výchozí hodnotu `petr`.
+Zadání 2: Na tlačíko přidejte událost `onClick`, která nastaví stav `login` na prázdný řetězec,
+ čímž se vyprázdní i políčko s loginem.
+*/
 
 export const Ukol3 = () => {
   const [login, setLogin] = useState('petr');
